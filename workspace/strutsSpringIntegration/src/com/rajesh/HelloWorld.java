bd9b6740f724bdc5df6beccb66185d763213a206
prashanth1230 <prashanth1230@gmail.com>
2012-04-22 14:27:29 +0530

    Working

---
+++ workspace/strutsSpringIntegration/src/com/rajesh/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.rajesh;
+
+public class HelloWorld {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String execute() {
+        return "SUCCESS";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
